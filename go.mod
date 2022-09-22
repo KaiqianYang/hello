25448d24c6d92c16a676df01961a3013033f455c
kaiqianyang <kaiqianyang@microsoft.com>
2022-09-22 20:24:48 +0800

    init codes

---
+++ go.mod
@@ -0,0 +1,11 @@
+module hello
+
+go 1.19
+
+require (
+	github.com/sergi/go-diff v1.1.0
+	github.com/vmware-labs/yaml-jsonpath v0.3.2
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
+)
+
+require github.com/dprotaso/go-yit v0.0.0-20191028211022-135eb7262960 // indirect
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+
+	//"time"
+	//"sync"
+	//"regexp"
+
+	"github.com/sergi/go-diff/diffmatchpatch"
+	"github.com/vmware-labs/yaml-jsonpath/pkg/yamlpath"
+	"gopkg.in/yaml.v3"
+)
+
+func main() {
+	y := `---
+		apiVersion: apps/v1
+		kind: Deployment
+		metadata:
+		name: sample-deployment
+		spec:
+		template:
+			spec:
+			containers:
+			- name: nginx
+				image: nginx
+				ports:
+				- containerPort: 80
+			- name: nginy
+				image: nginy
+				ports:
+				- containerPort: 81
+		`
+	var n yaml.Node
+
+	err := yaml.Unmarshal([]byte(y), &n)
+	if err != nil {
+		log.Fatalf("cannot unmarshal data: %v", err)
+	}
+
+	p, err := yamlpath.NewPath("$..spec.containers[*].image")
+	if err != nil {
+		log.Fatalf("cannot create path: %v", err)
+	}
+
+	q, err := p.Find(&n)
+	if err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+
+	for _, i := range q {
+		i.Value = "example.com/user/" + i.Value
+	}
+
+	var buf bytes.Buffer
+	e := yaml.NewEncoder(&buf)
+	defer e.Close()
+	e.SetIndent(2)
+
+	err = e.Encode(&n)
+	if err != nil {
+		log.Printf("Error: cannot marshal node: %v", err)
+		return
+	}
+
+	z := `apiVersion: apps/v1
+		kind: Deployment
+		metadata:
+		name: sample-deployment
+		spec:
+		template:
+			spec:
+			containers:
+			- name: nginx
+				image: example.com/user/nginx
+				ports:
+				- containerPort: 80
+			- name: nginy
+				image: example.com/user/nginy
+				ports:
+				- containerPort: 81
+		`
+	if buf.String() == z {
+		fmt.Printf("success")
+	} else {
+		dmp := diffmatchpatch.New()
+		diffs := dmp.DiffMain(buf.String(), z, false)
+		fmt.Println(diffs)
+	}
+}
